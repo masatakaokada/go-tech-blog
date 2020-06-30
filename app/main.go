b9a893486f6c1e7c96acc9f371664ad2a3398231
Masataka Okada <masataka.okada1117@gmail.com>
2020-07-01 07:35:40 +0900

    ルーティングの追加

--- app/main.go
+++ app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
      "net/http"
+     "strconv"
      "time"
 
      "github.com/flosch/pongo2"
@@ -16,7 +17,10 @@ var e = createMux()
 
 func main() {
      // `/` というパス（URL）と `articleIndex` という処理を結びつける
-	e.GET("/", articleIndex)
+     e.GET("/", articleIndex)
+     e.GET("/new", articleNew)
+     e.GET("/:id", articleShow)
+     e.GET("/:id/edit", articleEdit)
 
      // Webサーバーをポート番号 8080 で起動する
 	e.Logger.Fatal(e.Start(":8080"))
@@ -40,10 +44,43 @@ func createMux() *echo.Echo {
 
 func articleIndex(c echo.Context) error {
      data := map[string]interface{}{
-          "Message": "Hello, World!",
+          "Message": "Article Index",
           "Now":     time.Now(),
      }
      return render(c, "article/index.html", data)
+}
+
+func articleNew(c echo.Context) error {
+     data := map[string]interface{}{
+          "Message": "Article New",
+          "Now":     time.Now(),
+     }
+
+     return render(c, "article/new.html", data)
+}
+     
+func articleShow(c echo.Context) error {
+     id, _ := strconv.Atoi(c.Param("id"))
+
+     data := map[string]interface{}{
+          "Message": "Article Show",
+          "Now":     time.Now(),
+          "ID":      id,
+     }
+
+     return render(c, "article/show.html", data)
+}
+
+func articleEdit(c echo.Context) error {
+     id, _ := strconv.Atoi(c.Param("id"))
+
+     data := map[string]interface{}{
+          "Message": "Article Edit",
+          "Now":     time.Now(),
+          "ID":      id,
+     }
+
+     return render(c, "article/edit.html", data)
 }
 
 // pongo2 を利用して、テンプレートファイルとデータから HTML を生成
